f33c285b0c04f861f5f92b45b965907954a403c4
Nikita Ivanov <krasota156@gmail.com>
2024-09-02 11:26:57 +0300

    [bpjson] Add optional field support

--- bpjson/bpjson/bpjson.hpp
+++ bpjson/bpjson/bpjson.hpp
@@ -8,282 +8,326 @@
 
 #pragma once
 #include <bpacs/bpacs.hpp>
+#include <map>
 #include <optional>
 #include <stdexcept>
 #include <string>
-#include <map>
 
 namespace bpjson
 {
-    template<class Json>
+    enum class missing_fields
+    {
+        ignore,
+        default_initialize,
+        throw_exception
+    };
+
+    struct serialization_settings
+    {
+        missing_fields missing_fields_mode = missing_fields::throw_exception;
+    };
+
+    template <class Json>
     struct json_traits;
-    
-    template<class Json>
+
+    template <class Json>
     struct basic_json_traits
     {
         using json_type = Json;
     };
-    
-    template<class Json>
+
+    template <class Json>
     struct basic_cpp_json_traits : basic_json_traits<Json>
     {
         using json_type = Json;
-        
-        template<class S>
-        static json_type& get_any_subkey(json_type& json, S key)
+
+        template <class S>
+        static json_type &get_any_subkey(json_type &json, S key)
         {
             return json[key];
         }
-        
-        template<class S>
-        static const json_type& get_any_subkey(const json_type& json, S key)
+
+        template <class S>
+        static const json_type &get_any_subkey(const json_type &json, S key)
         {
             return json[key];
         }
-        
-        static const void copy(json_type& to, const json_type& from)
+
+        static const void copy(json_type &to, const json_type &from)
         {
             to = from;
         }
     };
-    
-    template<std::size_t KeyIndex, std::size_t ValueIndex>
+
+    template <std::size_t KeyIndex, std::size_t ValueIndex>
     struct basic_structured_binding_kv_traits
     {
-        template<class KV>
-        static auto& get_kp_key(const KV& kv)
+        template <class KV>
+        static auto &get_kp_key(const KV &kv)
         {
             return std::get<KeyIndex>(kv);
         }
-        
-        template<class KV>
-        static auto& get_kp_value(const KV& kv)
+
+        template <class KV>
+        static auto &get_kp_value(const KV &kv)
         {
             return std::get<ValueIndex>(kv);
         }
     };
-    
-	template<class Json, class T, typename = std::void_t<>>
-	struct json_walker
-	{
-		static void read(const Json& json, T& to)
-		{
-            to = json_traits<Json>::template get_typed_value<T>(json);
-		}
 
-		static void write(Json& json, const T& from)
-		{
-            json_traits<Json>::template write_typed_value<T>(json, from);
-		}
-	};
-
-	template<class Json>
-	struct json_serializer
-	{
-		template<class T>
-		static void read_to(const Json& json, T& to)
-		{
-			if constexpr (bpacs::has_bp_reflection<T>::value)
-				read_object(json, to);
-			else
-				json_walker<Json, T>::read(json, to);
-		}
-
-		template<class T>
-		static void write_from(Json& json, const T& from)
-		{
-			if constexpr (bpacs::has_bp_reflection<T>::value)
-				write_object(json, from);
-			else
-				json_walker<Json, T>::write(json, from);
-		}
-
-		template<class T>
-		static void read_object(const Json& json, T& object)
-		{
-			bpacs::iterate_object(
-				object,
-				[&json](auto field)
-				{
-					try
-					{
-						json_serializer<Json>::read_to(json_traits<Json>::get_existing_subkey(json, field.name()), field.value());
-					}
-					catch(const std::exception& e)
-					{
-						throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") + field.name() + "' caught exception - " + e.what());
-					}
-					catch(...)
-					{
-						throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") + field.name() + "' caught unknown exception");
-					}
-				}
-			);
-		}
-
-		template<class T>
-		static void write_object(Json& json, const T& object)
-		{
-			bpacs::iterate_object(
-				object,
-				[&json](auto field)
-				{
-                    try
-                    {
-                        json_serializer<Json>::write_from(json_traits<Json>::get_any_subkey(json, field.name()), field.value());
-                    }
-                    catch(const std::exception& e)
-                    {
-                        throw std::runtime_error(std::string("bpjson::json_serializer.write_object: field '") + field.name() + "' caught exception - " + e.what());
-                    }
-                    catch(...)
-                    {
-                        throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") + field.name() + "' caught unknown exception");
-                    }
-
-				}
-			);
-		}
-
-		template<class T>
-		static T read(const Json& json)
-		{
-			T value;
-			read_to(json, value);
-			return value;
-		}
-
-		template<class T>
-		static Json write(const T& value)
-		{
-			Json json;
-			write_from(json, value);
-			return json;
-		}
-	};
-
-    template<class Json>
-    struct json_walker<Json, std::string>
+    template <class T>
+    struct json_fields_optional : std::false_type
     {
-        static void read(const Json& json, std::string& to)
+    };
+
+    struct basic_json_walker
+    {
+        static constexpr bool force_optional = false;
+    };
+
+    template <class Json, class T, typename = std::void_t<>>
+    struct json_walker : basic_json_walker
+    {
+        static void read(const Json &json, T &to)
+        {
+            to = json_traits<Json>::template get_typed_value<T>(json);
+        }
+
+        static void write(Json &json, const T &from)
+        {
+            json_traits<Json>::template write_typed_value<T>(json, from);
+        }
+    };
+
+    template <class Json>
+    struct json_serializer
+    {
+        template <class T>
+        static void read_to(const Json &json, T &to, const serialization_settings &settings = {})
+        {
+            if constexpr (bpacs::has_bp_reflection<T>::value)
+                read_object(json, to, settings);
+            else
+                json_walker<Json, T>::read(json, to);
+        }
+
+        template <class T>
+        static void write_from(Json &json, const T &from, const serialization_settings &settings = {})
+        {
+            if constexpr (bpacs::has_bp_reflection<T>::value)
+                write_object(json, from, settings);
+            else
+                json_walker<Json, T>::write(json, from);
+        }
+
+        template <class T>
+        static void read_object(const Json &json, T &object, const serialization_settings &settings = {})
+        {
+            bpacs::iterate_object(object, [&json, &settings](auto field) {
+                try
+                {
+                    if ((settings.missing_fields_mode == missing_fields::default_initialize &&
+                         !json_walker<Json, T>::force_optional && !json_fields_optional<T>::value) ||
+                        json_traits<Json>::subkey_exists(json, field.name()))
+                    {
+                        json_serializer<Json>::read_to(json_traits<Json>::get_existing_subkey(json, field.name()),
+                                                       field.value(), settings);
+                    }
+                    else
+                    {
+                        if (settings.missing_fields_mode == missing_fields::default_initialize)
+                            field.value() = {};
+                    }
+                }
+                catch (const std::exception &e)
+                {
+                    throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") +
+                                             field.name() + "' caught exception - " + e.what());
+                }
+                catch (...)
+                {
+                    throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") +
+                                             field.name() + "' caught unknown exception");
+                }
+            });
+        }
+
+        template <class T>
+        static void write_object(Json &json, const T &object, const serialization_settings &settings = {})
+        {
+            bpacs::iterate_object(object, [&json, &settings](auto field) {
+                try
+                {
+                    json_serializer<Json>::write_from(json_traits<Json>::get_any_subkey(json, field.name()),
+                                                      field.value(), settings);
+                }
+                catch (const std::exception &e)
+                {
+                    throw std::runtime_error(std::string("bpjson::json_serializer.write_object: field '") +
+                                             field.name() + "' caught exception - " + e.what());
+                }
+                catch (...)
+                {
+                    throw std::runtime_error(std::string("bpjson::json_serializer.read_object: field '") +
+                                             field.name() + "' caught unknown exception");
+                }
+            });
+        }
+
+        template <class T>
+        static T read(const Json &json, const serialization_settings &settings = {})
+        {
+            T value;
+            read_to(json, value, settings);
+            return value;
+        }
+
+        template <class T>
+        static Json write(const T &value, const serialization_settings &settings = {})
+        {
+            Json json;
+            write_from(json, value, settings);
+            return json;
+        }
+    };
+
+    template <class Json>
+    struct json_walker<Json, std::string> : basic_json_walker
+    {
+        static void read(const Json &json, std::string &to)
         {
             to = json_traits<Json>::template get_typed_value<std::string>(json);
         }
-        
-        static void write(Json& json, const std::string& from)
+
+        static void write(Json &json, const std::string &from)
         {
             json_traits<Json>::template write_typed_value<std::string>(json, from);
         }
     };
-    
-	template<class T, typename = std::void_t<>>
-	class container_appender
-	{
-	public:
-		using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
 
-		container_appender(T& container)
-			: _container(&container)
-		{}
+    template <class T, typename = std::void_t<>>
+    class container_appender
+    {
+    public:
+        using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
 
-		void append(const value_type& value)
-		{
-			_container->push_back(value);
-		}
-	private:
-		T* _container;
-	};
+        container_appender(T &container) : _container(&container)
+        {
+        }
 
-	template<class T>
-	class container_appender<T, std::void_t<decltype(std::tuple_size<T>::value)>>
-	{
-	public:
-		using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
+        void append(const value_type &value)
+        {
+            _container->push_back(value);
+        }
 
-		container_appender(T& container)
-			: _container(&container)
-		{}
+    private:
+        T *_container;
+    };
 
-		void append(const value_type& value)
-		{
-			if (index < std::tuple_size_v<T>)
-				(*_container)[index++] = value;
-		}
-	private:
-		T* _container;
-		size_t index = 0;
-	};
+    template <class T>
+    class container_appender<T, std::void_t<decltype(std::tuple_size<T>::value)>>
+    {
+    public:
+        using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
 
-	template<class Json, class T>
-	struct json_walker<Json, T, std::void_t<decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))>>
-	{
-		using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
+        container_appender(T &container) : _container(&container)
+        {
+        }
 
-		static void read(const Json& json, T& to)
-		{
-			container_appender appender{ to };
-			for (auto& item : json)
-				appender.append(json_serializer<Json>::template read<value_type>(item));
-		}
+        void append(const value_type &value)
+        {
+            if (index < std::tuple_size_v<T>)
+                (*_container)[index++] = value;
+        }
 
-		static void write(Json& json, const T& from)
-		{
-			for (auto& item : from)
-				json_traits<Json>::add_array_element(json, json_serializer<Json>::template write<value_type>(item));
-		}
-	};
+    private:
+        T *_container;
+        size_t index = 0;
+    };
 
-	template<class Json, class T>
-	struct json_walker<Json, std::optional<T>>
-	{
-		static void read(const Json& json, std::optional<T>& to)
-		{
+    template <class Json, class T>
+    struct json_walker<Json, T,
+                       std::void_t<decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))>>
+        : basic_json_walker
+    {
+        using value_type = std::decay_t<decltype(*std::begin(std::declval<T>()))>;
+
+        static void read(const Json &json, T &to)
+        {
+            container_appender appender{to};
+            for (auto &item : json)
+                appender.append(json_serializer<Json>::template read<value_type>(item));
+        }
+
+        static void write(Json &json, const T &from)
+        {
+            for (auto &item : from)
+                json_traits<Json>::add_array_element(json, json_serializer<Json>::template write<value_type>(item));
+        }
+    };
+
+    template <class Json, class T>
+    struct json_walker<Json, std::optional<T>> : basic_json_walker
+    {
+        static constexpr bool force_optional = true;
+
+        static void read(const Json &json, std::optional<T> &to)
+        {
             if (!json_traits<Json>::is_null(json))
-				to = json_serializer<Json>::template read<T>(json);
-			else
-				to = std::nullopt;
-		}
+                to = json_serializer<Json>::template read<T>(json);
+            else
+                to = std::nullopt;
+        }
 
-		static void write(Json& json, const std::optional<T>& from)
-		{
-			if (from.has_value())
-				json_serializer<Json>::write_from(json, *from);
-			else
+        static void write(Json &json, const std::optional<T> &from)
+        {
+            if (from.has_value())
+                json_serializer<Json>::write_from(json, *from);
+            else
                 json_traits<Json>::make_null(json);
-		}
-	};
+        }
+    };
 
-	template<class Json>
-	struct json_walker<Json, Json>
-	{
-		static void read(const Json& json, Json& to)
-		{
+    template <class Json>
+    struct json_walker<Json, Json> : basic_json_walker
+    {
+        static void read(const Json &json, Json &to)
+        {
             json_traits<Json>::copy(to, json);
-		}
+        }
 
-		static void write(Json& json, const Json& from)
-		{
+        static void write(Json &json, const Json &from)
+        {
             json_traits<Json>::copy(json, from);
-		}
-	};
+        }
+    };
 
-	template<class Json, class T>
-	struct json_walker<Json, std::map<std::string, T>>
-	{
-		static void read(const Json& json, std::map<std::string, T>& to)
-		{
-			for (auto& j : json_traits<Json>::get_keys(json))
+    template <class Json, class T>
+    struct json_walker<Json, std::map<std::string, T>> : basic_json_walker
+    {
+        static void read(const Json &json, std::map<std::string, T> &to)
+        {
+            for (auto &j : json_traits<Json>::get_keys(json))
             {
                 auto key = json_traits<Json>::get_kp_key(j);
                 auto subkey = json_traits<Json>::get_existing_subkey(json, key);
                 json_serializer<Json>::read_to(json_traits<Json>::get_kp_value(j), to[key]);
             }
-		}
+        }
 
-		static void write(Json& json, const std::map<std::string, T>& from)
-		{
-			for (auto& pair : from)
-				json_serializer<Json>::write_from(json_traits<Json>::get_any_subkey(json, pair.first), pair.second);
-		}
-	};
-}
+        static void write(Json &json, const std::map<std::string, T> &from)
+        {
+            for (auto &pair : from)
+                json_serializer<Json>::write_from(json_traits<Json>::get_any_subkey(json, pair.first), pair.second);
+        }
+    };
+} // namespace bpjson
+
+#define BPJSON_ALL_FIELDS_OPTIONAL(type)                                                                               \
+    namespace bpjson                                                                                                   \
+    {                                                                                                                  \
+        template <>                                                                                                    \
+        struct json_fields_optional<type> : std::true_type                                                             \
+        {                                                                                                              \
+        };                                                                                                             \
+    }
--- bpjson/bpjson/nlohmann_traits.hpp
+++ bpjson/bpjson/nlohmann_traits.hpp
@@ -8,113 +8,115 @@
 #pragma once
 #include <nlohmann/json.hpp>
 
-#define BPJSON_NLOHMANN_BASIC_JSON_TPL_DECL                                \
-    template<template<typename, typename, typename...> class ObjectType,   \
-        template<typename, typename...> class ArrayType,                   \
-        class StringType, class BooleanType, class NumberIntegerType,     \
-        class NumberUnsignedType, class NumberFloatType,                   \
-        template<typename> class AllocatorType,                            \
-        template<typename, typename = void> class JSONSerializer>
+#define BPJSON_NLOHMANN_BASIC_JSON_TPL_DECL                                                                            \
+    template <template <typename, typename, typename...> class ObjectType,                                             \
+              template <typename, typename...> class ArrayType, class StringType, class BooleanType,                   \
+              class NumberIntegerType, class NumberUnsignedType, class NumberFloatType,                                \
+              template <typename> class AllocatorType, template <typename, typename = void> class JSONSerializer>
 
-#define BPJSON_NLOHMANN_BASIC_JSON_TPL                                     \
-    nlohmann::basic_json<ObjectType, ArrayType, StringType, BooleanType,             \
-        NumberIntegerType, NumberUnsignedType, NumberFloatType,                    \
-        AllocatorType, JSONSerializer>
+#define BPJSON_NLOHMANN_BASIC_JSON_TPL                                                                                 \
+    nlohmann::basic_json<ObjectType, ArrayType, StringType, BooleanType, NumberIntegerType, NumberUnsignedType,        \
+                         NumberFloatType, AllocatorType, JSONSerializer>
 
 namespace bpjson
 {
     BPJSON_NLOHMANN_BASIC_JSON_TPL_DECL
-    struct json_traits<BPJSON_NLOHMANN_BASIC_JSON_TPL>
-    : basic_cpp_json_traits<BPJSON_NLOHMANN_BASIC_JSON_TPL>
+    struct json_traits<BPJSON_NLOHMANN_BASIC_JSON_TPL> : basic_cpp_json_traits<BPJSON_NLOHMANN_BASIC_JSON_TPL>
     {
         using json_type = BPJSON_NLOHMANN_BASIC_JSON_TPL;
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        template<class T>
-        static T get_typed_value(const json_type& json)
+
+        template <class T>
+        static T get_typed_value(const json_type &json)
         {
             return json.template get<T>();
         }
-        
-        template<class T>
-        static void write_typed_value(json_type& json, const T& value)
+
+        template <class T>
+        static void write_typed_value(json_type &json, const T &value)
         {
             json = value;
         }
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        template<class S>
-        static json_type& get_existing_subkey(json_type& json, S key)
+
+        template <class S>
+        static bool subkey_exists(const json_type &json, S key)
+        {
+            return json.find(key) != json.end();
+        }
+
+        template <class S>
+        static json_type &get_existing_subkey(json_type &json, S key)
         {
             return json.at(key);
         }
-        
-        template<class S>
-        static const json_type& get_existing_subkey(const json_type& json, S key)
+
+        template <class S>
+        static const json_type &get_existing_subkey(const json_type &json, S key)
         {
             return json.at(key);
         }
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        template<class S>
-        static json_type& get_any_subkey(json_type& json, S key)
+
+        template <class S>
+        static json_type &get_any_subkey(json_type &json, S key)
         {
             return json[key];
         }
-        
-        template<class S>
-        static const json_type& get_any_subkey(const json_type& json, S key)
+
+        template <class S>
+        static const json_type &get_any_subkey(const json_type &json, S key)
         {
             return json[key];
         }
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        static void add_array_element(json_type& json, const json_type& element)
+
+        static void add_array_element(json_type &json, const json_type &element)
         {
             json.push_back(element);
         }
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        static bool is_null(const json_type& json)
+
+        static bool is_null(const json_type &json)
         {
             return json.is_null();
         }
-        
-        static void make_null(json_type& json)
+
+        static void make_null(json_type &json)
         {
             json = {};
         }
-        
+
         ////////////////////////////////////////////////////////////////////
-        
-        static auto get_keys(json_type& json)
+
+        static auto get_keys(json_type &json)
         {
             return json.items();
         }
-        
-        static auto get_keys(const json_type& json)
+
+        static auto get_keys(const json_type &json)
         {
             return json.items();
         }
-        
-        template<class KV>
-        static auto& get_kp_key(const KV& kv)
+
+        template <class KV>
+        static auto &get_kp_key(const KV &kv)
         {
             return kv.key();
         }
-        
-        template<class KV>
-        static auto& get_kp_value(const KV& kv)
+
+        template <class KV>
+        static auto &get_kp_value(const KV &kv)
         {
             return kv.value();
         }
     };
-}
+} // namespace bpjson
 
 // Cleanup
 #undef BPJSON_NLOHMANN_BASIC_JSON_TPL
